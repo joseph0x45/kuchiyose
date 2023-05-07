17bdd96765f8fac8f928bcd7c66ea39f1a0976d1
TheWisePigeon <zozozozeph@gmail.com>
2023-05-07 18:24:06 +0000

    feat: add kitty template

--- bin.js
+++ bin.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+//console.log("Welcome to kuchiyose. Contribute to this package at https://github.com/TheWisePigeon/kuchiyose")
 
-console.log("Welcome to kuchiyose. Contribute to this package at https://github.com/TheWisePigeon/kuchiyose")
-console.log(__dirname)
+const path = require("path")
+const TEMPLATES_PATH = path.join(__dirname, "/templates")
+
+const args = process.argv
+const is_interactive = args[2] === "inter" || false
---
+++ utils.js
@@ -0,0 +1,12 @@
+const templates = [
+    {
+        name:"kitty",
+        description:"Barebones Sveltekit and tailwind project",
+        author:"thewisepigeon <https://github.com/TheWisePigeon>",
+        tags:["sveltekit", "svelte", "tailwind"]
+    }
+]
+
+export default {
+    templates
+}
