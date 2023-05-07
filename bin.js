12805053500d73884779b6a86a0a31bdef210f2a
TheWisePigeon <zozozozeph@gmail.com>
2023-05-07 22:49:27 +0000

    feat: stable release 1

--- bin.js
+++ bin.js
@@ -16,30 +16,53 @@ const TEMPLATES_PATH = join(__dirname, "/templates")
 const args = process.argv
 const is_interactive = args[2] === "inter" || false
 
-if(is_interactive){
+if (is_interactive) {
     console.log(`Welcome to kuchiyose. Contribute to this package at ${repo_url}`)
     process.exit(0)
 }
 
 const template_name = args[2]
 const app_dir = args[3]
-if(!templates.find(template=> template.name===template_name)){
+if (!templates.find(template => template.name === template_name)) {
     console.log(`No template named ${template_name}!`)
     console.log(`See list of templates at ${repo_url}/templates.md`)
     process.exit(0)
 }
 
+if (app_dir) {
+    try {
+        console.log("Copying files")
+        copySync(`${TEMPLATES_PATH}/${template_name}`, app_dir)
+        if (app_dir === ".") {
+            console.log('Project created')
+            console.log('Now run `pnpm/npm/yarn install` and happy coding!')
+            console.log('Contribute to kuchiyose and add your own templates at ')
+            process.exit(0)
+        }
+        console.log("Copying files")
+        copySync(`${TEMPLATES_PATH}/${template_name}`, app_dir)
+        console.log(`Your project was created at ${app_dir}`)
+        console.log(`Now run cd ${app_dir}, pnpm/npm/yarn install and happy coding!`)
+        process.exit(0)
+
+    } catch (_) {
+        console.log("Something went wrong creating your project")
+        console.log(`Please report a bug at ${issues_url}`)
+        process.exit(0)
+    }
+}
+
 inquirer
     .prompt([
         {
-            type:"input",
-            name:"app_dir",
-            message:"Where do you want to create your project? (Leave blank or use . to create in current directory)"
+            type: "input",
+            name: "app_dir",
+            message: "Where do you want to create your project? (Leave blank or use . to create in current directory)"
         }
     ])
-    .then((answer)=>{
+    .then((answer) => {
         try {
-            if(answer.app_dir===""||answer.app_dir==="."){
+            if (answer.app_dir === "" || answer.app_dir === ".") {
                 console.log("Copying files")
                 copySync(`${TEMPLATES_PATH}/${template_name}`, '.')
                 console.log('Project created')
