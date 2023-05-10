65c4fa17a77b8a8239ea36a4bff7d5c82fe2d38d
TheWisePigeon <zozozozeph@gmail.com>
2023-05-10 11:11:50 +0000

    ref: remove rex templates & add express

--- utils.js
+++ utils.js
@@ -1,20 +1,15 @@
 export const templates = [
-    {
-        name:"kitty",
-        description:"Barebones Sveltekit and tailwind project",
-        author:"thewisepigeon <https://github.com/TheWisePigeon>",
-        tags:["sveltekit", "svelte", "tailwind"]
-    },
   {
-    name:"rex-ts",
+    name:"kitty",
+    description:"Barebones Sveltekit and tailwind project",
+    author:"thewisepigeon <https://github.com/TheWisePigeon>",
+    tags:["sveltekit", "svelte", "tailwind"]
+  },
+  {
+    name:"express-ts",
     description:"Barebones express and TypeScript project",
     author:"thewisepigeon <https://github.com/TheWisePigeon>",
     tags: ["typescript", "express"]
-  },  {
-    name:"rex-js",
-    description:"Barebones express and JavaScript project",
-    author:"thewisepigeon <https://github.com/TheWisePigeon>",
-    tags: ["javascript", "express"]
   }
 ]
 
