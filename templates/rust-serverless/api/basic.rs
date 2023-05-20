25806b6ff7819006c53429013acc241a51232bb1
TheWisePigeon <zozozozeph@gmail.com>
2023-05-20 07:06:54 +0000

    feat: add rust serverless template

---
+++ templates/rust-serverless/api/basic.rs
@@ -0,0 +1,13 @@
+use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
+       .status(StatusCode::OK)
+       .header("Content-Type","application/json")
+       .body(json!({ "hello":"world" }).to_string().into())?)
+}
