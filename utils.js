dbfc7faecd61c1dec0bee45869e67c25c9f22703
Ara <oladipoeyiara@gmail.com>
2023-07-18 11:54:37 -0400

    Adding vue template

--- utils.js
+++ utils.js
@@ -1,33 +1,38 @@
 export const templates = [
   {
-    name:"kitty",
-    description:"Barebones Sveltekit and tailwind project",
-    author:"thewisepigeon <https://github.com/TheWisePigeon>",
-    tags:["sveltekit", "svelte", "tailwind"]
+    name: "kitty",
+    description: "Barebones Sveltekit and tailwind project",
+    author: "thewisepigeon <https://github.com/TheWisePigeon>",
+    tags: ["sveltekit", "svelte", "tailwind"],
   },
   {
-    name:"express-ts",
-    description:"Barebones express and TypeScript project",
-    author:"thewisepigeon <https://github.com/TheWisePigeon>",
-    tags: ["typescript", "express"]
+    name: "express-ts",
+    description: "Barebones express and TypeScript project",
+    author: "thewisepigeon <https://github.com/TheWisePigeon>",
+    tags: ["typescript", "express"],
   },
   {
-    name:"rust-serverless",
-    description:"Vercel Rust serverless functions",
-    author:"thewisepigeon <https://github.com/TheWisePigeon>",
-    tags: ["rust", "vercel", "serverless functions"]
+    name: "rust-serverless",
+    description: "Vercel Rust serverless functions",
+    author: "thewisepigeon <https://github.com/TheWisePigeon>",
+    tags: ["rust", "vercel", "serverless functions"],
   },
   {
-    name:"go-serverless",
-    description:"Vercel Go serverless functions",
-    author:"thewisepigeon <https://github.com/TheWisePigeon>",
-    tags: ["go", "vercel", "serverless functions"]
+    name: "go-serverless",
+    description: "Vercel Go serverless functions",
+    author: "thewisepigeon <https://github.com/TheWisePigeon>",
+    tags: ["go", "vercel", "serverless functions"],
   },
   {
-    name:"npm-basic-ts",
-    description:"Simple TypeScript npm package",
-    author:"thewisepigeon <https://github.com/TheWisePigeon>",
-    tags: ["typescript", "npm", "npm package", "nodejs"]
-  }
-]
-
+    name: "npm-basic-ts",
+    description: "Simple TypeScript npm package",
+    author: "thewisepigeon <https://github.com/TheWisePigeon>",
+    tags: ["typescript", "npm", "npm package", "nodejs"],
+  },
+  {
+    name: "vue-ts",
+    description: "Barebones Vue and tailwind project",
+    author: "ara-o <https://github.com/Ara-O>",
+    tags: ["vue", "vite", "typescript", "tailwind"],
+  },
+];
